10e8fc347d7f6e701fb3aa02c2d40a43beeea00a
yuraej <yuraej79@gmail.com>
2019-09-30 21:05:06 +0300

    commit2

--- src/test/java/model/SearchItem.java
+++ src/test/java/model/SearchItem.java
@@ -1,19 +1,19 @@
 package model;
 
-public class SearchItem {
-    public String getSearchString() {
-        return searchString;
-    }
+import java.util.Scanner;
 
+public class SearchItem {
     public void setSearchString(String searchString) {
         this.searchString = searchString;
     }
 
     private String searchString;
 
+    public String getSearchString() {
+        return searchString;
+    }
+
     public SearchItem(String searchString) {
         this.searchString = searchString;
     }
-
-    
 }
